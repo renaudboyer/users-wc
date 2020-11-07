348a0a8c2bb8afe0bfd1acb85b1ec0b5584595d0
Renaud BOYER <rboyer@linagora.com>
2020-11-07 18:11:42 +0100

    Add user add button

--- user-module.js
+++ user-module.js
@@ -3,11 +3,27 @@ class UserModule extends HTMLElement {
         super();
 
         // Create a shadowDOM
-        const shadowRootElement = this.attachShadow({ mode: 'open'});
+        this.shadowRootElement = this.attachShadow({ mode: 'open'});
 
         // Define component HTML code
         // (We could also have used js DOM method as createElement ...)
-        shadowRootElement.innerHTML = this.getTemplate();
+        this.shadowRootElement.innerHTML = this.getTemplate();
+
+        // Add the event listener
+        // NOTE: We register 'this.addItem.bind(this)' instead of 'this.addItem'
+        //   because in Javascript 'this' in a function depends on the calling context
+        //   bind() allow to force the value of 'this' inside the function
+        const addButton = this.shadowRootElement.querySelector('.btn-add');
+        addButton.addEventListener('click', this.addItem.bind(this));
+    }
+
+    addItem() {
+        const userInput = this.shadowRootElement.querySelector('#username');
+        const userListElement = this.shadowRootElement.querySelector('user-list');
+        const userItemElement = document.createElement('user-item');
+        userItemElement.innerText = userInput.value;
+
+        userListElement.appendChild(userItemElement);
     }
 
     getTemplate() {
@@ -17,6 +33,7 @@ class UserModule extends HTMLElement {
     <label for="username">User</label>
     <input id="username" type="text">
 </form>
+<button class="btn-add">Add</button>
 
 <user-list>
     <user-item>Leanne Graham</user-item>
