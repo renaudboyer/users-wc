46be45aca0cf3cbe57aaef4b9a54d720c088bee2
Renaud BOYER <rboyer@linagora.com>
2020-11-07 12:30:59 +0100

    Add HTML template for userItem element

--- user-item.js
+++ user-item.js
@@ -1,3 +1,21 @@
-class UserItem extends HTMLElement {}
+class UserItem extends HTMLElement {
+    constructor() {
+        super();
+
+        // Create a shadowDOM
+        const shadowRootElement = this.attachShadow({ mode: 'open'});
+
+        // Define component HTML code
+        // (We could also have used js DOM method as createElement ...)
+        shadowRootElement.innerHTML = `
+<li>
+<slot></slot>
+</li>
+`;
+        // <slot> element allows to define where the content of the user-item will be placed
+        // <user-item>userName</user-item> will compute into
+        // <li>userName</li>
+    }
+}
 
 customElements.define('user-item', UserItem)
